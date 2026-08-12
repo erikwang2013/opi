4b6c328e73db822519a9acd0ce19c8cc6f0333ef
erik <erik@erik.xyz>
2026-08-12 17:31:17 +0800

    feat(android): OpiImeService InputMethodService with FlutterView

---
+++ flutter/app/android/app/src/main/kotlin/io/opi/input/OpiImeService.kt
@@ -0,0 +1,64 @@
+package io.opi.input
+
+import android.inputmethodservice.InputMethodService
+import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import io.flutter.embedding.android.FlutterView
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.dart.DartExecutor
+import io.flutter.injector.FlutterInjector
+import io.flutter.plugin.common.MethodChannel
+
+/** OPI IME 宿主：FlutterView 作为输入视图，Dart imeMain entrypoint。 */
+class OpiImeService : InputMethodService() {
+    private var flutterEngine: FlutterEngine? = null
+    private var channel: MethodChannel? = null
+
+    override fun onCreateInputView(): View {
+        val engine = FlutterEngine(this)
+        val entrypoint = DartExecutor.DartEntrypoint(
+            FlutterInjector.instance().flutterLoader().findAppBundlePath(),
+            "imeMain"
+        )
+        engine.dartExecutor.executeDartEntrypoint(entrypoint)
+
+        val view = FlutterView(
+            this,
+            FlutterView.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+        )
+        view.attachToFlutterEngine(engine)
+
+        channel = MethodChannel(engine.dartExecutor.binaryMessenger, "opi/ime")
+        channel?.setMethodCallHandler { call, result ->
+            when (call.method) {
+                "commitText" -> {
+                    currentInputConnection?.commitText(call.arguments as String, 1)
+                    result.success(null)
+                }
+                "deleteBackward" -> {
+                    currentInputConnection?.deleteSurroundingText(1, 0)
+                    result.success(null)
+                }
+                "performEnter" -> {
+                    currentInputConnection?.performEditorAction(EditorInfo.IME_ACTION_SEND)
+                    result.success(null)
+                }
+                else -> result.notImplemented()
+            }
+        }
+
+        flutterEngine = engine
+        return view
+    }
+
+    override fun onDestroy() {
+        flutterEngine?.destroy()
+        flutterEngine = null
+        channel = null
+        super.onDestroy()
+    }
+}
